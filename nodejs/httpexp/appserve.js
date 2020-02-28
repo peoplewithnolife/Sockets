51bace13fe328d9c84102fe2a9510745c1d18d55
Phil S <pschlosser@nkhome.com>
2020-02-28 16:05:51 -0500

    trying stuff

--- nodejs/httpexp/appserve.js
+++ nodejs/httpexp/appserve.js
@@ -4,25 +4,28 @@ const bodyParser = require("body-parser");
 const port = 3000;
 
 const app = express();
+//app.use(bodyParser);
+app.use(bodyParser.urlencoded({extended: true}));
+//app.use(express.static("public"));
 
 var getsRcvd = 0;
 var postsRcvd = 0;
 
-app.param('test', function(req,res,test) {
-    console.log("Param");
-    next();
-});
+//app.param('test', function(req,res,test) {
+//    console.log("Param: " + req.param);
+//    //next();
+//});
 
 app.get("/:test", function (req, res) {
     res.send("Get a job");
-    //console.log(req);
+    console.log(req.param);
     console.log(req.body);
     console.log(getsRcvd);
     //rsp.sendFile(__dirname + "/index.html");
     getsRcvd++;
 });
 
-app.post("/:test", function (req,res) {
+app.post("/:testtest", function (req,res) {
    res.send("Post a job");
    //console.log(req);
    console.log(req.params);
